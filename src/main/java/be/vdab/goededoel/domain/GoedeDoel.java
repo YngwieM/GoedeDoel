e79a36edeb5935c79125b44b3af67f882acb50e3
yngwie <yngwie_mariman@hotmail.com>
2021-03-11 11:02:55 +0100

    Jenkins 7

--- src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
@@ -23,4 +23,13 @@ public class GoedeDoel {
 	public BigDecimal getOpbrengst() {
 		return opbrengst;
 	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object instanceof GoedeDoel) {
+			GoedeDoel ander = (GoedeDoel) object;
+			return naam.equalsIgnoreCase(ander.naam);
+		}
+		return false;
+	}
 }
